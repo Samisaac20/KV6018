8282bd334affc09235b2002116cad34d45e9ca92
samisaac2023 <s4mioliver@gmail.com>
2025-12-11 22:28:11 +0000

    added files for placement and started random search algo

---
+++ backend/algorithms/cargo_container_loading.py
@@ -0,0 +1,2 @@
+# will act as main for algorithms package
+# from here call GA/ Greedys/ Brute force or random etc
---
+++ backend/algorithms/random_cargo.py
@@ -0,0 +1,31 @@
+import random
+# from placement_algorithm import Placement_Algorithm
+
+class Random_Search:
+    def __init__(self, container, cargo_items, max_iterations=1000):
+        self.container = container
+        self.cargo_items = cargo_items
+        self.max_iterations = max_iterations
+        self.best_arrangement = None
+        self.best_filled_volume = 0
+
+    def run(self):
+        
+        for _ in range(self.max_iterations):
+            random.shuffle(self.cargo_items)
+            current_arrangement = []
+            current_filled_volume = 0
+
+            for item in self.cargo_items:
+                if self.container.can_place(item):
+                    self.container.place(item)
+                    current_arrangement.append(item)
+                    current_filled_volume += item.volume
+
+            if current_filled_volume > self.best_filled_volume:
+                self.best_filled_volume = current_filled_volume
+                self.best_arrangement = current_arrangement.copy()
+
+            self.container.reset()
+
+        return self.best_arrangement, self.best_filled_volume
